ca2d37eaecf4ec4f1f2d13e53ca01baab44b64d0
vanskym <vanskym635@mail.sandhills.edu>
2025-01-29 10:05:05 -0500

    Add files via upload

---
+++ src/labs/example/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package labs.example.hello;
+
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+package labs.example.hello;
+
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/labs/example/mammal/Mammal.java
@@ -0,0 +1,38 @@
+package src.labs.example.mammals;
+
+public class Mammal {
+    protected String hairColor;
+    protected String eyeColor;
+    protected String bodyTemp;
+    protected String skinType;
+    protected String eyeType;
+    protected String hairType;
+
+    public void mammal(String name){
+    }
+        public void setName(String name){
+            this.name = name;
+        }
+        
+        public void run(){
+                System.out.println("The " + this.name + "is now running!");
+            }
+        public void eat(){
+                System.out.println("The " + this.name + "is now eating!");
+            }
+            static void sleep(){
+            }
+            static void scratch(){
+            }
+            static void play(){
+            }
+            static void hunt(){
+            }
+            static void hide(){
+            }
+            static void fight(){
+            }
+            static void relax(){
+            }
+        
+    }
