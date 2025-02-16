8ff4ead823352d1972721852697aaf08b23d97c1
vanskym <vanskym635@mail.sandhills.edu>
2025-02-16 17:46:35 -0500

    Add files via upload

---
+++ bin/labs/example/physics/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main extends Physics{
+    public static void main(String[] args) {
+        Physics physics = new Physics();
+        physics.getDistance();
+        physics.getVelocity();
+        physics.getMomentum();
+        physics.getForce();
+        physics.getWork();
+        physics.getKineticEnergy();
+        physics.getPotentialEnergy();
+    }
+}
