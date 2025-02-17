43f95453ed802469e57057b9684e96f69ebcc586
SANDHILLS\LH218-18 <jad.talbert@gmail.com>
2025-02-17 11:38:20 -0500

    4

---
+++ bin/labs/example/physics/ThetaWIP.java
@@ -0,0 +1,19 @@
+public class ThetaWIP {
+    
+}
+/* this is just a small place I can place the work for how theta works
+ * 
+ * the dog will walk 3 miles to the south and four miles to the east, these two angles perpendicular 
+ * you use the pythagorean theorem 
+ * this would end up being (3*3) + (4*4) = 9 + 16
+ * this ends up becoming 25, which you would then find that numbers square root which will end up becoming '5'
+ * 
+ * so therefore the distance traveled between these two points ends up being '5'
+ * 
+ * then there will be finding the theta of this number i don't know how this works, will need to look at soon
+ * 
+ * for the lab, we will be seeing if this number created is greater than 37 degrees or less than 36.87 degrees
+ * you will be using this to check whether it is a true right angle or not and this will need to be given out
+ * 
+ * https://www.geeksforgeeks.org/how-to-find-an-angle-in-a-right-angled-triangle-2/
+ */
