37ad900cf7a9e766a11d5ea721bb99efb8213b5f
olgand <st.olgand@gmail.com>
2025-03-09 19:41:21 -0400

    3/9/2025 - 1

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -3,37 +3,36 @@ package labs.example.arrays;
 import labs.example.arrays.ArrayOperations;
 
 public class ArrayOperations {
-    int number1 = 6;
-
-    public void createNewArray(int number1){
         
-        int arrayNum[] = new int[number1];
+    public static void main(String[] args) {
+        final int number1 = 6;
+        int[] arrayNum = new int[6];
+        createNewArray(arrayNum);
+        displayArray(arrayNum, number1);
+        for (int index = 0; index < arrayNum.length; index++)
+        {
+            System.out.print(arrayNum[index] + " ");     
+        }   
+}
+    public static void createNewArray(int arrayNum[]){
         /* i will be making the current count of the loop (index) into the corresponding element of the array
-         * example, on the second loop, array will begin to look like = [1, 2, null, null... etc]
-         * to pass an argument of an array into a method, this is talked about on page 465
+         * example, on the SECOND for loop, array will begin to look like = [1, 2, null, null... etc]
+         *
          */
         for (int index = 0; index < arrayNum.length; index++)
         {
-            arrayNum[index] = index;
-        }
+            arrayNum[index] = index;   
+        }     
+    }
+
+    private static void displayArray(int arrayNum[], int number1){
+
+        System.out.println("I created a new array and it now has " + number1 + " items in it.");  
+        System.out.println("The array items and their values are listed below: ");  
         
+
     }
 
 
-    private void displayArray(){
 
-        ArrayOperations array1 = new ArrayOperations();
-        array1.createNewArray();
-        System.out.println("I created a new array and it now has " + array1+ " items in it.");  
-    }
-
-
-    public static void main(String[] args) {
-        ArrayOperations array2 = new ArrayOperations();
-        array2.displayArray();
-
-
-
-
-}
 }
