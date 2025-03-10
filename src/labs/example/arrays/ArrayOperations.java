d04c70b0dd21e17af8df23f1ce50ab257a2c437c
olgand <st.olgand@gmail.com>
2025-03-09 20:36:09 -0400

    3/9/2025-2

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -11,8 +11,15 @@ public class ArrayOperations {
         displayArray(arrayNum, number1);
         for (int index = 0; index < arrayNum.length; index++)
         {
-            System.out.print(arrayNum[index] + " ");     
+            System.out.print(arrayNum[index] + " ");  
         }   
+        getDaysAndMonths();
+
+        /*ArrayOperations bubble = new ArrayOperations();
+        int number3[] = {1, 2, 3, 4, 5};
+        bubble.sortArrays(number3);*/
+       
+
 }
     public static void createNewArray(int arrayNum[]){
         /* i will be making the current count of the loop (index) into the corresponding element of the array
@@ -32,7 +39,29 @@ public class ArrayOperations {
         
 
     }
+    public static void getDaysAndMonths(){
+        System.out.println("\n"); 
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"};
+    for ( int i = 0; i < 12; i++){
+        System.out.println("There are " + days[i] + " days in " + months[i]);
+    }
+    }
 
+        void sortArrays(int numberLarge[])
+        {
+            int sorting = numberLarge.length;
+          /* get length of large array */
+            for (int i = 0; i < sorting - 1; i++)
+            /* this is a nested for loop, the second index or i2 will be compared with the first */
+                for (int i2 = 0; i2 < sorting - i - 1; i2++)
 
-
+                    if (numberLarge[i2] > numberLarge[i2 + 1]) {
+                      /* this will change the numbers, putting the smaller one first */
+                        int numberChange = numberLarge[i2];
+                        numberLarge[i2] = numberLarge[i2 + 1];
+                        numberLarge[i2 + 1] = numberChange;
+                    }
+        }
 }
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -1,6 +1,29 @@
+package labs.example.arrays;
+
+
+
+public class Main extends ArrayOperations{
+    public static void main(String[] args) {
+        ArrayOperations ob = new ArrayOperations();
+        int numberLarge[] = {3, 2, 45, 6, 32, 6, 8, /* began adding more nums at this point */ 10, 12, 11, 90, 60, 45,
+            81, 5, 24, 44, 42, 8, 41, 18, 49, 27, 57, 96, 93, 61, 70, 40, 22, 20, 92, 93, 95, 25, 2, 97, 7, 96, 31,
+            37, 45, 70, 89, 64, 21, 34, 17, 64, 3, 73, 47, 30};
+      
+        ob.sortArrays(numberLarge);
+      
+          int sorted = numberLarge.length;
+      
+        for (int i = 0; i < sorted; ++i)
+            System.out.print(numberLarge[i] + " ");
+        System.out.println();
+
 /* for number 2 of our lab we will be doing something called a bubble sort
  * this is not modern but it is important to understand the way array works
  * 
  * this will be done by a for loop. we will loop through each of the ints in the array, and each time it goes through the for loop counter
  * it will see whether or not the number is higher than the previous one. the number will be placed in the front accordingly. 
  */
+
+
+    }
+}
